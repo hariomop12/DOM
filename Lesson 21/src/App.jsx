904ab34c940976fc44ab397d1b05579367547152
hariom <Hariomvirkhare02@gmail.com>
2026-04-09 10:33:32 +0530

    Loading & Error States (Advanced)  [Panding]

---
+++ Lesson 21/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-slate-500 h-screen w-screen flex items-center justify-center">
+      <h1 className="text-4xl text-white">Panding But Soon</h1>
+    </div>
+  );
+}
+
+export default App;
